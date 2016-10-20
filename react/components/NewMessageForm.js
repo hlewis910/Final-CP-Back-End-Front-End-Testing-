b3583eaced3a32019f8436e6656eb4e28235bd55
queerviolet <ashi@queerlyamorous.org>
2016-10-20 13:32:08 -0400

    Strip form state management

--- react/components/NewMessageForm.js
+++ react/components/NewMessageForm.js
@@ -1,31 +1,6 @@
 import React from 'react';
 
 export default class extends React.Component {
-
-    constructor() {
-        super();
-        this.state = {
-            recipient: '',
-            subject: '',
-            body: ''
-        };
-        this.updateRecipient = this.updateRecipient.bind(this);
-        this.updateSubject = this.updateSubject.bind(this);
-        this.updateBody = this.updateBody.bind(this);
-    }
-
-    updateRecipient(e) {
-        this.setState({ recipient: e.target.value });
-    }
-
-    updateSubject(e) {
-        this.setState({ subject: e.target.value });
-    }
-
-    updateBody(e) {
-        this.setState({ body: e.target.value });
-    }
-
     render() {
         return (
             <form>
